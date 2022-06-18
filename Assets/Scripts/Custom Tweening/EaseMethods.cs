87f76555b3360fe53fced2d1930d752437106c28
peculiarnewbie <curiousarmand@gmail.com>
2022-06-18 12:59:17 +0700

    TextMeshPro import

--- Assets/Scripts/Custom Tweening/EaseMethods.cs
+++ Assets/Scripts/Custom Tweening/EaseMethods.cs
@@ -56,11 +56,13 @@ public class EaseMethods : MonoBehaviour
 					// Debug.Log(Mathf.Log(0.000005f, 10f));
 
 
-					var angle   = x * Mathf.PI * 1.0f * b;
-					var weightA = 1.0f - Mathf.Pow(x, 0.125f / s);
-					var weightB = 1.0f - Mathf.Pow(1.0f - x, 8.0f * s);
+					var angle   = x * Mathf.PI * 1.0f * s;
+					// var weightA = 1.0f - Mathf.Pow(x, 0.125f / s);
+					// var weightB = 1.0f - Mathf.Pow(1.0f - x, 8.0f * s);
+					double weightA = 1.0 - System.Math.Pow((double) x, 0.125 / (double) s);
+					double weightB = 1.0 - System.Math.Pow(1.0 - (double) x, 8.0 * (double) s);
 
-					x = Mathf.LerpUnclamped(0.0f, 1.0f - Mathf.Cos(angle) * weightA, weightB);
+					x = Mathf.LerpUnclamped(0.0f, 1.0f - Mathf.Cos(angle) * (float) weightA, (float) weightB);
 				}
 				break;
 
